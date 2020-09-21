e7385e3fabf069444113db45c1a746c20adea019
manojravichandran <dvmano18@gmail.com>
2020-09-21 16:12:41 +0530

    date

--- src/app/employees.service.ts
+++ src/app/employees.service.ts
@@ -9,7 +9,7 @@ export class EmployeeService {
 
   getEmployees(){
     return [
-      {"id":1,"name":"Manoj","lname":"Ravindran","age":20,"dob":"02/08/1998","Gender":"Male","salary":20000,"Phno":7610531489},
+      {"id":1,"name":"Manoj","lname":"Ravindran","age":20,"dob":"02.08.1998","Gender":"Male","salary":20000,"Phno":7610531489},
       {"id":2,"name":"Manju","lname":"maaran","age":40,"dob":"05/05/1996","Gender":"female","salary":70000,"Phno":9010045589},
       {"id":3,"name":"Mani","lname":"Raichandran","age":50,"dob":"02/06/1996","Gender":"Male","salary":60000,"Phno":8010031489},
       {"id":4,"name":"Mala","lname":"kohli","age":60,"dob":"03/07/1997","Gender":"feale","salary":30000,"Phno":9010006489},
